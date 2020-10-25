8e058209905797e2c94ab756f31305a9b7ff0506
PMJ <pmj@pmj.hu>
2020-10-26 00:51:04 +0100

    Update backend

--- app/index.js
+++ app/index.js
@@ -1,5 +1,8 @@
 const http = require('http');
+const url = require('url');
 const fs = require('fs');
+const path = require('path');
+
 const { Telegraf } = require('telegraf');
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const chatIdPath = process.env.DELIVERY_PATH;
@@ -7,19 +10,15 @@ const txsPath = process.env.TXS_PATH;
 let chatId = readChatId();
 
 bot.on('text', (ctx) => {
-    chatId = writeChatId(ctx.message.from.id);
-    return ctx.reply(`Freeze requests will be delivered to: ${ctx.message.from.username}`)
+    if(process.env.DELIVERY_ALLOW.split(':').includes(ctx.message.from.id.toString())){
+        chatId = writeChatId(ctx.message.from.id);
+        return ctx.reply(`Freeze requests will be delivered to: ${ctx.message.from.username}`)
+    }
 })
 
 
 
 const server = http.createServer((req, res) => {
-    if (req.method === 'GET') {
-        res.writeHead(200, {
-            'content-type': 'text/html'
-        });
-        return fs.createReadStream('index.html').pipe(res);
-    }
 
     if (req.method === 'POST') {
         const unixNano = + new Date();
@@ -31,10 +30,68 @@ const server = http.createServer((req, res) => {
                 });
             }
         });
-
+        return;
     }
-});
 
+    // parse URL
+    const parsedUrl = url.parse(req.url);
+    // extract URL path
+    let pathname = `.${parsedUrl.pathname}`;
+
+    // based on the URL path, extract the file extention. e.g. .js, .doc, ...
+    let ext = path.parse(pathname).ext;
+
+    if(pathname === './'){
+        pathname = './index.html';
+        ext = '.html';
+    }
+
+
+    // maps file extention to MIME typere
+    const map = {
+        '.ico': 'image/x-icon',
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.json': 'application/json',
+        '.css': 'text/css',
+        '.png': 'image/png',
+        '.jpg': 'image/jpeg',
+        '.wav': 'audio/wav',
+        '.mp3': 'audio/mpeg',
+        '.svg': 'image/svg+xml',
+        '.pdf': 'application/pdf',
+        '.doc': 'application/msword'
+    };
+
+    pathname = pathname.replace('./', './dist/');
+    console.log(`${req.method} ${req.url} => ${pathname}`);
+
+    fs.exists(pathname, function(exist) {
+        if (!exist) {
+            // if the file is not found, return 404
+            res.statusCode = 404;
+            res.end(`File ${pathname} not found!`);
+            return;
+        }
+
+        // if is a directory search for index file matching the extention
+        if (fs.statSync(pathname).isDirectory()) pathname += '/index' + ext;
+
+        // read file from file system
+        fs.readFile(pathname, function(err, data) {
+            if (err) {
+                res.statusCode = 500;
+                res.end(`Error getting the file: ${err}.`);
+            } else {
+                // if the file is found, set Content-type and send data
+                res.setHeader('Content-type', map[ext] || 'text/plain');
+                res.end(data);
+            }
+        });
+    });
+
+
+})
 bot.launch();
 server.listen(process.env.PORT || 3000);
 
@@ -50,3 +107,5 @@ function readChatId(){
     }
     return false;
 }
+
+console.log(`Server listening on port ${(process.env.PORT || 3000)}`);
